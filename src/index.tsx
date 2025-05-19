b7440fd2c3ecf63bf3d6e41317ec11689151b86b
moalamir52 <Mo.alamir52@gmail.com>
2025-05-19 10:43:05 +0400

    Initial upload

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Report from './report.tsx';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Report />
+  </React.StrictMode>
+);
