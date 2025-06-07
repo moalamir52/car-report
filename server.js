eebc6f0b8eee514cdb7f45df301820a474b09347
moalamir52 <Mo.alamir52@gmail.com>
2025-06-07 15:38:36 +0400

    your message

--- server.js
+++ server.js
@@ -37,4 +37,6 @@ app.get('/load', async (req, res) => {
   }
 });
 
-app.listen(4000, () => console.log('✅ API running on http://localhost:4000'));
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`✅ API running on port ${PORT}`));
+
